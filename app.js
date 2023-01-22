6b475c8951465cabeaaffa15263a0da462a7e2eb
Thujeevan <thujeevan2@gmail.com>
2023-01-22 12:06:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use('/', (req, res) => {
+  try {
+    res.json({message: "Hello, We are coming from express server, Happy to connect with you Flutter!"})
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.listen(8080, () => {
+  console.log("Backend is started on 8080")
+})
