c989354049ba2bfcee8f7c8fd655773cab4ca8c7
Thujeevan <thujeevan2@gmail.com>
2023-02-08 08:49:23 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const database = require("mongoose");
 exports.connectDatabase = async (req, res, next) => {
   try {
     // mongoose.set('strictQuery', true);
-    await database.connect("mongodb+srv://thujeevan:2728@cluster0.bj7ul.mongodb.net/service-crud?retryWrites=true&w=majority");
+    await database.connect(); // Add your database
     console.log("Database connected!");
     next();
   } catch (e) {
